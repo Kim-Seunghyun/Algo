7df47fa6fdd50e0702d5f21f01d9258a98eb2569
Kim-Seunghyun <63468607+Kim-Seunghyun@users.noreply.github.com>
2021-08-08 08:26:58 +0900

    Update 1859. 백만 장자 프로젝트(D2).java

--- SWExpertAcademy/1859. 백만 장자 프로젝트(D2).java
+++ SWExpertAcademy/1859. 백만 장자 프로젝트(D2).java
@@ -6,8 +6,6 @@ import java.io.InputStreamReader;
 public class Solution {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		// br = new BufferedReader(new InputStreamReader(new
-		// FileInputStream("input_1859.txt")));
 		int T = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
 		for (int i = 1; i <= T; i++) {
@@ -15,13 +13,14 @@ public class Solution {
 			long res = 0;
 			int N = Integer.parseInt(br.readLine());
 			String[] arr = br.readLine().split(" ");
+			//입력완료
 			int size = arr.length - 2;
 			int prev = Integer.parseInt(arr[size + 1]);
 			while (size >= 0) {
 				int next = Integer.parseInt(arr[size]);
-				if (prev > next)
+				if (prev > next)	//다음가격이 이전보다 크다면 수익을 저장
 					res = res + (prev - next);
-				else {
+				else {	//다음가격이 이전보다 작거나 같다면 이전값을 다음값으로 옮겨옴
 					prev = next;
 				}
 				size--;
