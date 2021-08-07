3f55e19cc4dfc54c3b497c8ba0c8ead53d8688a9
Kim-Seunghyun <63468607+Kim-Seunghyun@users.noreply.github.com>
2021-08-08 08:28:22 +0900

    Update 2805. 농작물 수확하기(D3).java

--- SWExpertAcademy/2805. 농작물 수확하기(D3).java
+++ SWExpertAcademy/2805. 농작물 수확하기(D3).java
@@ -13,20 +13,21 @@ public class Solution {
 			for (int j = 0; j < tc; j++) {
 				farm[j] = br.readLine().toCharArray();
 			}
+			//입력완료
 			int y = 0;
 			int x = (tc - 1) / 2;
 			int res = 0;
 			boolean flag = false;
 			for (int k = 0; k < tc; k++) {
-				if (flag == false) {
+				if (flag == false) {	//농장의 가로크기 도달전
 					for (int l = x - k; l <= x + k; l++) {
-						res = res + farm[k][l] - '0';
+						res = res + farm[k][l] - '0';	//늘려가기
 					}
-					if (k == x)
+					if (k == x)	//농장의 가로크기 도달
 						flag = true;
 				} else if (flag == true) {
 					for (int l = x - (tc - k - 1); l <= x + (tc - k - 1); l++) {
-						res = res + farm[k][l] - '0';
+						res = res + farm[k][l] - '0';	//줄여가기
 					}
 				}
 			}
